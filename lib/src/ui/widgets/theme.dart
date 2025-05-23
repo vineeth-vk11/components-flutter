3e20a52eb60a9afd2ffefe63e5bac4385298b04d
vineethvk11 <f20180233@hyderabad.bits-pilani.ac.in>
2025-05-23 10:14:58 +0530

    fixed bug with DialogTheme used for DialogThemeData type

--- lib/src/ui/widgets/theme.dart
+++ lib/src/ui/widgets/theme.dart
@@ -84,7 +84,7 @@ class LiveKitTheme {
             return Colors.white.withValues(alpha: 0.3);
           }),
         ),
-        dialogTheme: DialogTheme(
+        dialogTheme: DialogThemeData(
           backgroundColor: cardColor,
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(8),
